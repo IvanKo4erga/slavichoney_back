b68e035776efc151c270a5f9a908feb9fa2e461b
Kargin Daniil 1-МД-19 <74720514+IvanKo4erga@users.noreply.github.com>
2025-04-08 23:54:19 +0300

    Add files via upload

--- slavichoney_back/wsgi.py
+++ slavichoney_back/wsgi.py
@@ -12,9 +12,9 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# path = '/home/kocherga/slavichoney'
-# if path not in sys.path:
-#     sys.path.append(path)
+path = '/home/kocherga/slavichoney'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slavichoney_back.settings')
 
